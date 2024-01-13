09f08c4f2858f4532fa3df8bcf01d2e605178293
LeeJunkyu <dlwnsrb0216@gmail.com>
2024-01-13 19:10:38 +0900

    Test: unit 정상작동 test code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image'
+'use client'
+
+import {useEffect} from "react";
 
 export default function Home() {
+
   return (
-    <div className='text-50'>tailwind-Test</div>
+    <>
+    <div className='text-200'>tailwind-Test</div>
+
+    <div className='text-50'>50</div>
+    <div className='text-100'>100</div>
+    </>
   )
 }
