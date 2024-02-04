fa085812d1976874609eead6d5ee06773c65d6ba
LeeJunkyu <dlwnsrb0216@gmail.com>
2024-02-05 00:02:45 +0900

    Feat: Get api Test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,22 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+import { GET } from '@/app/api/fetcher';
+
 export default function Home() {
+  const fetchTestResult = useQuery({
+    queryKey: ['fetchTestResult'],
+    queryFn: async () => {
+      const response = await GET('/testApi.json');
+
+      return response.json();
+    },
+  });
+
+  const { data: testResult } = fetchTestResult;
+
+  console.log('result', testResult);
+
   return (
     <>
       <div className="text-200">tailwind-Test</div>
