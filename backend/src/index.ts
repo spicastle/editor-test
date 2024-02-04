ff05dd961493280bfbe2098f9649e17272f28376
금희성 <smrgd88@gmail.com>
2024-02-04 10:13:08 +0900

    sonarlint fix, || -> ??

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
